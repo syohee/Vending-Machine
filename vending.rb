22f95a90d5f697ea0ca70381c35c02e6f9e6ec56
syohee <tjdgmll2@naver.com>
2018-11-15 13:10:04 +0900

    1. 메인화면 : 메뉴와 음료 목록 표시 2. 돈을 넣으면 구입 가능한 음료 목록이 표시되고, 돈을 더 넣을 것인지 물어봄

--- vending.rb
+++ vending.rb
@@ -1,7 +1,7 @@
 class Vending
 
   def initialize()
-    @total = 0    #投入金額の総計
+    $total = 0    #投入金額の総計
     @sales = 0    #売り上げ金額
     @money = [10, 50, 100, 500, 1000]  #投入可能お金
     $drink = [$cola, $redbull, $water] #drink 種類
@@ -10,56 +10,74 @@ class Vending
 
   #投入
  def input_money()
-
    print "input money : "
    user_money = gets.chomp
    user_money = user_money.to_i
-
    if @money.include?(user_money)
-     puts "投入金額 : " + (@total += user_money).to_s
-     stock_check(@total)
-     if @stock == true
-       return
-     else
-       puts "投入金額不足"
-       input_money()
-     end
+     $total += user_money
+     puts "total money : " + $total.to_s
+     stock_check()
    else
-     puts "釣り銭 : " + user_money.to_s
+     puts "money error - repay : " + user_money.to_s
+     input_money()
    end
  end
 
   #払い戻し操作
  def output_money
-     puts "釣り銭 : " + @total
+     puts "釣り銭 : " + $total
  end
 
- def stock_check(total)
+ def stock_check()
    $drink.each{ |v|
-      if v.stock > 0 && v.price <= total
-        puts "購入可能 : " + v.name.to_s
+      if v.stock > 0 && v.price <= $total
         @stock = true
-      else
-        puts "購入不可"
-        return
       end }
+    if @stock == true
+      while true
+        drink_list()
+        print "To put more money? (yes or no) : "
+        answer = gets.chomp
+        if answer == "yes"
+          input_money()
+          return
+        elsif answer == "no"
+          return
+        else
+          puts "answer error"
+        end
+      end
+    else
+      input_money()
+    end
  end
 
  def buy()
-   stock_check(@total)
+   stock_check($total)
    print "select drink : "
    drink_name = gets.chomp
 
    $drink.each{ |v|
      if v.name == drink_name
-       if v.stock > 0 && v.price <= @total
+       if v.stock > 0 && v.price <= $total
          v.set_stock v.stock - 1
          @sales += v.price
-         @total -= v.price
-         puts "釣り銭 : " + (@total).to_s
-         @total = 0
+         $total -= v.price
+         puts "釣り銭 : " + ($total).to_s
+         $total = 0
        end
      end}
+ end
+
+ def drink_list()
+   puts "┌───drink list───┐"
+   $drink.each{|v|
+     if v.stock > 0 && v.price <= $total
+       puts v.name.to_s + " [" + v.price.to_s + "] : 購入可能"
+     else
+       puts v.name.to_s + " [" + v.price.to_s + "] : 購入不可"
+     end}
+   puts "└────────────────┘"
  end
 
 end
